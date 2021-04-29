84bbf7879351b5e5de72e6f76f3974a417f27be2
JAtoms <nzube1994@gmail.com>
2021-04-29 02:10:46 +0100

    Card input readjusted

--- lib/constants.dart
+++ lib/constants.dart
@@ -1,10 +1,28 @@
 import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
 
-const activeCardColor = Color(0XFF1D1E33);
-const inactiveCardColor = Color(0XFFF111328);
-const bottomCardColor = Color(0XFFEB1555);
+const kActiveCardColor = Color(0XFF1D1E33);
+const kInactiveCardColor = Color(0XFFF111328);
+const kSliderInactiveCardColor = Color(0XFF8D8E98);
+const kBottomCardColor = Color(0XFFEB1555);
 
-const labelTestStyle = TextStyle(
+const kLabelTestStyle = TextStyle(
   fontSize: 18,
   color: Color(0XFF8D8E98),
+);
+
+const kLabelBodyTestStyle = TextStyle(
+  fontSize: 20,
+  color: Colors.white,
+);
+
+const kLabelMediumTestStyle = TextStyle(
+  fontSize: 20,
+  color: Color(0XFF8D8E98),
+);
+
+const kLabelBoldTestStyle = TextStyle(
+  fontSize: 50,
+  fontWeight: FontWeight.bold,
+  color: Colors.white,
 );
--- lib/input_page_file.dart
+++ lib/input_page_file.dart
@@ -1,11 +1,11 @@
 import 'package:bmi/reusableCards.dart';
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'constants.dart';
 import 'icon_content.dart';
 
 enum Gender { male, female }
+enum Value { add, subtract }
 
 Widget appBar() {
   return AppBar(title: Text('BMI CALCULATOR'), centerTitle: true);
@@ -16,6 +16,9 @@ class InputPage extends StatefulWidget {
   _InputPageState createState() => _InputPageState();
 }
 
+int bmiValue = 120;
+int ageValue = 25;
+int weightValue = 120;
 Gender selectedGender;
 
 class _InputPageState extends State<InputPage> {
@@ -24,10 +27,12 @@ class _InputPageState extends State<InputPage> {
     return Scaffold(
       appBar: appBar(),
       body: Column(
+        crossAxisAlignment: CrossAxisAlignment.stretch,
         children: [
           Expanded(
             flex: 8,
             child: Column(
+              crossAxisAlignment: CrossAxisAlignment.stretch,
               children: [
                 Expanded(
                   flex: 2,
@@ -46,8 +51,8 @@ class _InputPageState extends State<InputPage> {
                             });
                           },
                           color: (selectedGender == Gender.male)
-                              ? activeCardColor
-                              : inactiveCardColor,
+                              ? kActiveCardColor
+                              : kInactiveCardColor,
                           cardChild: GenderWidget(
                             gender: 'MALE',
                             genderIcon: FontAwesomeIcons.mars,
@@ -67,8 +72,8 @@ class _InputPageState extends State<InputPage> {
                             });
                           },
                           color: (selectedGender == Gender.female)
-                              ? activeCardColor
-                              : inactiveCardColor,
+                              ? kActiveCardColor
+                              : kInactiveCardColor,
                           cardChild: GenderWidget(
                             gender: 'FEMALE',
                             genderIcon: FontAwesomeIcons.venus,
@@ -85,7 +90,56 @@ class _InputPageState extends State<InputPage> {
                     mTop: 25,
                     mRight: 30,
                     mBottom: 0,
-                    color: activeCardColor,
+                    color: kActiveCardColor,
+                    cardChild: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Padding(
+                          padding: const EdgeInsets.fromLTRB(0, 15, 0, 12),
+                          child: Text(
+                            'HEIGHT',
+                            style: kLabelTestStyle,
+                          ),
+                        ),
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          crossAxisAlignment: CrossAxisAlignment.baseline,
+                          textBaseline: TextBaseline.ideographic,
+                          children: [
+                            Text(
+                              bmiValue.toString(),
+                              style: kLabelBoldTestStyle,
+                            ),
+                            Padding(
+                              padding: const EdgeInsets.all(6.0),
+                              child: Text(
+                                'cm',
+                                style: kLabelMediumTestStyle,
+                              ),
+                            ),
+                          ],
+                        ),
+                        Padding(
+                          padding: const EdgeInsets.fromLTRB(18, 18, 18, 0),
+                          child: SliderTheme(
+                            data: SliderTheme.of(context).copyWith(
+                                thumbColor: kBottomCardColor,
+                                overlayColor: Color(0X29EB1555),
+                                activeTrackColor: Colors.white),
+                            child: Slider(
+                              value: bmiValue.toDouble(),
+                              min: 120.0,
+                              max: 220.0,
+                              onChanged: (double newValue) {
+                                setState(() {
+                                  bmiValue = newValue.toInt();
+                                });
+                              },
+                            ),
+                          ),
+                        )
+                      ],
+                    ),
                   ),
                 ),
                 Expanded(
@@ -99,7 +153,39 @@ class _InputPageState extends State<InputPage> {
                           mTop: 25,
                           mRight: 0,
                           mBottom: 15,
-                          color: activeCardColor,
+                          color: kActiveCardColor,
+                          cardChild: Column(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: [
+                              Text(
+                                'WEIGHT',
+                                style: kLabelTestStyle,
+                              ),
+                              Text(
+                                weightValue.toString(),
+                                style: kLabelBoldTestStyle,
+                              ),
+                              Row(
+                                mainAxisAlignment: MainAxisAlignment.center,
+                                children: [
+                                  RoundButton(
+                                      icon: Icon(
+                                    FontAwesomeIcons.plus,
+                                    color: Colors.white,
+                                  )),
+                                  SizedBox(
+                                    width: 12,
+                                  ),
+                                  RoundButton(
+                                    icon: Icon(
+                                      FontAwesomeIcons.minus,
+                                      color: Colors.white,
+                                    ),
+                                  ),
+                                ],
+                              )
+                            ],
+                          ),
                         ),
                       ),
                       Expanded(
@@ -109,7 +195,7 @@ class _InputPageState extends State<InputPage> {
                           mTop: 25,
                           mRight: 30,
                           mBottom: 15,
-                          color: activeCardColor,
+                          color: kActiveCardColor,
                         ),
                       ),
                     ],
@@ -125,11 +211,36 @@ class _InputPageState extends State<InputPage> {
               mTop: 16,
               mRight: 0,
               mBottom: 0,
-              color: bottomCardColor,
+              color: kBottomCardColor,
+              cardChild: Center(
+                child: Text(
+                  'CALCULATE YOUR BMI',
+                  style: kLabelBodyTestStyle,
+                ),
+              ),
             ),
           )
         ],
       ),
+    );
+  }
+}
+
+class RoundButton extends StatelessWidget {
+  final Widget icon;
+  final String value;
+
+  RoundButton({@required this.icon});
+
+  @override
+  Widget build(BuildContext context) {
+    return RawMaterialButton(
+      onPressed: () {},
+      fillColor: Color(0XFF4C4F5E),
+      shape: CircleBorder(),
+      elevation: 6,
+      constraints: BoxConstraints.tightFor(width: 50, height: 50),
+      child: icon,
     );
   }
 }
